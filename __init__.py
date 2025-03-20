4bcc0c4fd8b43acfac0e10b4f08ec3ccd535552b
sankarnag23 <iamsankarnag@gmail.com>
2025-03-20 22:18:20 +0000

    Fix deployment: Restructure as proper Python package

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from hotel_booking_system_v2 import UserInterfaceAgent, BookingAPIAgent, IntegrationAgent, RoomType
-from hotel_providers import HotelDataProvider
-from middleware import SecurityHeadersMiddleware, RequestLoggingMiddleware, RateLimitMiddleware, SQLInjectionMiddleware, XSSMiddleware
-from validators import BookingRequest, sanitize_search_params, validate_api_key, validate_hotel_id, sanitize_log_data 
